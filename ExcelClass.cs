c92194264cfbd6e7cfd931c4ada46ac8a9e3da76
alejandrovaras <70179469+alejandrovaras@users.noreply.github.com>
2020-11-27 12:07:29 -0500

    Add files via upload

---
+++ ExcelClass.cs
@@ -0,0 +1,39 @@
+﻿// Programmer: Alejandro Varas
+// Date: 11/27/2020
+// Project: Class Tech Project
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace Form2
+{
+    class ExcelClass
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public ExcelClass(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            i++;
+            j++;
+            if (ws.Cells[i, j].Value2 != null)
+                return ws.Cells[i, j].Value2;
+            else
+                return "";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        public void OpenFile()
+        {
+            ExcelClass excel = new ExcelClass(@"C:\Users\varas\OneDrive\Documents\2019-2020NBAPlayerStats.xlsx", 1);
+
+            MessageBox.Show(excel.ReadCell(2, 2));
+        }
+    }
+}
